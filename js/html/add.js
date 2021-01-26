db13e65b6797241cf3a36c707af03a9af06d0e5e
garubi <garubi0404@gmail.com>
2021-01-27 04:28:06 +0900

    add save

--- js/html/add.js
+++ js/html/add.js
@@ -1,3 +1,5 @@
+
+
 (function(){
     const voca_form = document.getElementById('form');
 
@@ -5,11 +7,14 @@
 
     function submit(event){
         alert("테스트!");
-        //event.preventDefault();
+        event.preventDefault();
+        voca_text();
+        mean_text();
+        save();
     }
-    
 })()
-let count = 1;
+
+var count = 1;
 
 function add(){
     var newDIV = document.createElement("div");
@@ -18,7 +23,6 @@ function add(){
     newDIV.innerHTML = '<input type="text" name="voca" class="voca" placeholder="단어" required> <input type="text" name="mean" class="mean" placeholder="뜻" required> <input type="button" value="추가하기" onclick="add()" class="voca_input"> <input type="button" value="제거하기" onclick="remove(this)">' 
     parent.appendChild(newDIV);
     count++;
-    console.log(count);
 }
 
 function remove(p){
@@ -29,15 +33,39 @@ function remove(p){
     var parent = p.parentElement;
     parent.parentNode.removeChild(parent);
     count--;
-    console.log(count);
 }
 
-function title_text(){
+function save(){
     let titlet = document.getElementById('title').value;
-    console.log(titlet);
+    let detailt = document.getElementById('detail').value;
+    length = document.getElementsByClassName('voca').length;
+    EZ.storage.noteList.push({
+        name: titlet,
+        detail: detailt,
+        wordList: vocat,
+        meanList: meant
+    })
 }
 
-function detail_text(){
-    let detailt = document.getElementById('detail').value;
-    console.log(detailt);
+
+function voca_text(){
+    length = document.getElementsByClassName('voca').length;
+    vocat = [];
+    for(var i=0; i<length; i++){
+        vocat.push(document.getElementsByClassName('voca')[i].value);
+    }
+    for(var i=0; i<length; i++){
+        console.log(vocat[i]);
+    }
+}
+
+function mean_text(){
+    length = document.getElementsByClassName('voca').length;
+    meant = [];
+    for(var i=0; i<length; i++){
+        meant.push(document.getElementsByClassName('mean')[i].value);
+    }
+    for(var i=0; i<length; i++){
+        console.log(meant[i]);
+    }
 }
